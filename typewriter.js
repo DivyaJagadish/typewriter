5f587067e951d3f3a235e5e28c4f256a50e447b8
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-09 21:27:27 +0000

    changes

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,13 @@
+// implement a typewriter animation logic
 const sentence = "hello there from lighthouse labs";
 let i = 0;
 for (const char of sentence) {
+  // For animated printing increment i with every looping
   setTimeout(() => {
-    process.stdout.write(char);
-  },i);
+    process.stdout.write(char);// prints the value in same line .
+  }, i);
   i += 50;
 };
-setTimeout(()=>{
-console.log("");
-},i);
+setTimeout(() => {
+  console.log("");
+}, i);
